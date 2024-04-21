3c6b6f468e5f85ac7ef204b976b4e6885992e853
lyuanzhi <lyuanzhi862066195@outlook.com>
2024-04-20 22:56:09 -0400

    add env

--- environment-16.py
+++ environment-16.py
@@ -22,15 +22,7 @@ class Environment:
         self.SERVEBASE = [-1 for _ in range(0, self.num_of_points_measured)]
         self.SERVEBASE[0] = 6
 
-        self.rss_max = -10
-        self.rss_min = -100
-        self.rss_range = self.rss_max - self.rss_min
-        self.sinr_max = 40
-        self.sinr_min = -20
-        self.sinr_range = self.sinr_max - self.sinr_min
-        self.d_max = 1800
-        self.d_min = 0
-        self.d_range = self.d_max - self.d_min
+        self.eps = np.finfo(np.float32).eps.item()
 
         self.eNB_coordinate = np.zeros([self.num_of_eNBs, 2])
 
@@ -54,23 +46,25 @@ class Environment:
     def step(self, A, CURRENT_point):
 
         done = False
-        if A == 0:
-            self.SERVEBASE[CURRENT_point + 1] = self.SERVEBASE[CURRENT_point]
-        elif A == 1:
-            self.SERVEBASE[CURRENT_point + 1] = np.argmax(self.SINR[:, CURRENT_point])
+
+        self.SERVEBASE[CURRENT_point + 1] = A
 
         rss = self.noisy_RSS[self.SERVEBASE[CURRENT_point + 1]][CURRENT_point + 1]
         sinr = self.SINR[self.SERVEBASE[CURRENT_point + 1]][CURRENT_point + 1]
         distance = self.distance[self.SERVEBASE[CURRENT_point + 1]][CURRENT_point + 1]
-        s_ = [rss, sinr, distance]
-        reward = (rss - self.rss_min) / self.rss_range + (sinr - self.sinr_min) / self.sinr_range + (
-                    self.d_max - distance) / self.d_range
+        x = self.MS_coordinate[CURRENT_point][0]
+        y = self.MS_coordinate[CURRENT_point][1]
+        s_ = [rss, sinr, distance, x, y]
 
-        if A == 1:
-            reward -= 1.3
+        rss = (rss - self.noisy_RSS.mean()) / (self.noisy_RSS.std() + self.eps)
+        sinr = (sinr - self.SINR.mean()) / (self.SINR.std() + self.eps)
+        distance = (distance - self.distance.mean()) / (self.distance.std() + self.eps)
+        reward = rss + sinr - distance
 
-        CURRENT_point += 1
-        if CURRENT_point == self.num_of_points_measured - 1:
+        if self.SERVEBASE[CURRENT_point] != self.SERVEBASE[CURRENT_point + 1]:
+            reward -= 10.0
+
+        if CURRENT_point + 1 == self.num_of_points_measured - 1:
             done = True
 
         return s_, reward, done
@@ -80,18 +74,21 @@ class Environment:
         rss = self.noisy_RSS[0][0]
         sinr = self.SINR[0][0]
         distance = self.distance[0][0]
-        s = [rss, sinr, distance]
+        x = self.MS_coordinate[0][0]
+        y = self.MS_coordinate[0][1]
+        s = [rss, sinr, distance, x, y]
         return s
 
     def base_station_random_setting(self):
         for i in range(self.num_of_eNBs):
-            self.fc[i] = np.random.rand()*1000*10e6+6000*10e6
-            self.tx_RSS[i] = np.random.rand()*5+20
+            self.fc[i] = np.random.rand() * 1000 * 10e6 + 6000 * 10e6
+            self.tx_RSS[i] = np.random.rand() * 5 + 20
 
     def calculate_distance(self):
         for x in range(self.num_of_eNBs):
             for i in range(self.num_of_points_measured):
-                self.distance[x][i] = math.sqrt(pow(self.MS_coordinate[i][0] - self.eNB_coordinate[x][0], 2) + pow(self.MS_coordinate[i][1] - self.eNB_coordinate[x][1], 2))
+                self.distance[x][i] = math.sqrt(pow(self.MS_coordinate[i][0] - self.eNB_coordinate[x][0], 2) + pow(
+                    self.MS_coordinate[i][1] - self.eNB_coordinate[x][1], 2))
 
     def get_RSS(self):
         for i in range(self.num_of_points_measured):
@@ -145,9 +142,34 @@ class Environment:
         for t in range(5600, 6300):
             self.MS_coordinate[t] = self.MS_coordinate[t - 1] + np.array([0, unit])
 
+    def new_walk(self):
+        a = 20
+
+        ini_coordinate = [-799.9, 799.9]
+        unit = (a + 1) * 1600 / self.num_of_points_measured
+
+        x = int(self.num_of_points_measured / (a + 1) / (a - 1))
+        y = int(self.num_of_points_measured / (a + 1))
+        for t in range(y):
+            self.MS_coordinate[t] = np.array(ini_coordinate) + np.array([0, - unit * t])
+        for cnt in range(a - 1):
+            for b in range(y, x + y):
+                self.MS_coordinate[b + cnt * (x + y)] = self.MS_coordinate[b + cnt * (x + y) - 1] + np.array([unit, 0])
+            for b in range(x + y, x + 2 * y):
+                if cnt % 2 == 0:
+                    self.MS_coordinate[b + cnt * (x + y)] = self.MS_coordinate[b + cnt * (x + y) - 1] + np.array(
+                        [0, unit])
+                else:
+                    self.MS_coordinate[b + cnt * (x + y)] = self.MS_coordinate[b + cnt * (x + y) - 1] + np.array(
+                        [0, - unit])
+
+        for t in range((a - 1) * x + a * y, self.num_of_points_measured):
+            self.MS_coordinate[t] = self.MS_coordinate[t - 1] + np.array([0, -unit])
+
     def calculate_all_rand_walk(self):
         self.base_station_random_setting()
-        self.square_walk()
+        # self.square_walk()
+        self.new_walk()
         self.calculate_distance()
         self.get_RSS()
         self.calculate_SINR()
